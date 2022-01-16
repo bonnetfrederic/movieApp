8a8d676b7cc275345478c7b45d0c6c0ce8dd66d4
Frédéric Bonnet <frederic.bonnet.fr@gmail.com>
2022-01-16 23:02:28 +0100

    add ternary for vote_average icon

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 let movies = [];
 
-async function fetchMovies() {
-  await fetch("https://api.themoviedb.org/3/search/movie?api_key=4c1c0d1b5ab4b958dc6516de881a951d&query=star&language=fr-FR")
+async function fetchMovies(searchParam = "") {
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=4c1c0d1b5ab4b958dc6516de881a951d&query=${searchParam}&language=fr-FR`;
+  await fetch(url)
     .then((res) => res.json())
     .then((data) => movies = data.results);
 
@@ -22,11 +23,16 @@ function displayMovies() {
           <p id="movieReleaseDate">Date de sortie : ${movie.release_date}</p>
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" id="moviePic" alt="Movie Poster" width="100"px height="150px">
           <p id="movieSummary">${movie.overview}</p>
-          <p id="movieScore">Note moyenne : ${movie.vote_average}/10</p>
+          <p id="movieScore">Note moyenne : ${movie.vote_average}/10 <span><img id="scorePic" src="./assets/icons/${parseFloat(movie.vote_average) >= 6.5 ? 'flame.svg' : 'snowflake.svg'}"></span></p>
         </div>
       `
     })
     .join('');
 }
 
-window.addEventListener("load", fetchMovies());
+// window.addEventListener("load", fetchMovies());
+
+submitButton.addEventListener('click', (e) =>  {
+  e.preventDefault();
+  fetchMovies(searchInput.value);
+});
