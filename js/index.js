dc53720357df575d44bec70c7e69e2e9c79d87ca
Frédéric Bonnet <frederic.bonnet.fr@gmail.com>
2022-01-16 23:15:42 +0100

    manage generic-poster case

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ function displayMovies() {
         <div class="card">
           <h3>${movie.title}</h3>
           <p id="movieReleaseDate">Date de sortie : ${movie.release_date}</p>
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" id="moviePic" alt="Movie Poster" width="100"px height="150px">
+          <img src="${movie.poster_path == null ? './assets/img/generic-poster.png' : 'https://image.tmdb.org/t/p/w500'+ movie.poster_path}" id="moviePic" alt="Movie Poster" width="100"px height="150px">
           <p id="movieSummary">${movie.overview}</p>
           <p id="movieScore">Note moyenne : ${movie.vote_average}/10 <span><img id="scorePic" src="./assets/icons/${parseFloat(movie.vote_average) >= 6.5 ? 'flame.svg' : 'snowflake.svg'}"></span></p>
         </div>
